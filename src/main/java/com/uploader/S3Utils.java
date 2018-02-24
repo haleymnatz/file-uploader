2c08b21f643fde010542367660026c326ed99821
haleymnatz <haleymnatz@gmail.com>
2018-02-24 16:22:28 -0500

    Code cleanup for S3Utils and controller

--- src/main/java/com/uploader/S3Utils.java
+++ src/main/java/com/uploader/S3Utils.java
@@ -1,9 +1,5 @@
 package com.uploader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,7 +40,6 @@ public class S3Utils {
 		    System.err.println(e.getMessage());
 		    System.exit(1);
 		}
-		System.out.println("Done!");
     }
 	
 	public List<String> s3ListObjects() {
@@ -58,31 +53,15 @@ public class S3Utils {
 	}
 	
 	public S3ObjectInputStream s3DownloadObject(String keyName) {
-		
-//		try {
+		S3ObjectInputStream s3is = null;
+		try {
 		    S3Object o = s3Client.getObject(bucketName, keyName);
-		    S3ObjectInputStream s3is = o.getObjectContent();
-		    return s3is;
-//		    FileOutputStream fos = new FileOutputStream(new File(keyName));
-//		    System.out.println(s3is);
-//		    byte[] read_buf = new byte[1024];
-//		    int read_len = 0;
-//		    while ((read_len = s3is.read(read_buf)) > 0) {
-//		        fos.write(read_buf, 0, read_len);
-//		    }
-//		    s3is.close();
-//		    fos.close();
-//		    System.out.println("Downloaded!");
-//		} catch (AmazonServiceException e) {
-//		    System.err.println(e.getErrorMessage());
-//		    System.exit(1);
-//		} catch (FileNotFoundException e) {
-//		    System.err.println(e.getMessage());
-//		    System.exit(1);
-//		} catch (IOException e) {
-//		    System.err.println(e.getMessage());
-//		    System.exit(1);
-//		}
+		    s3is = o.getObjectContent();
+		} catch (AmazonServiceException e) {
+		    System.err.println(e.getErrorMessage());
+		    System.exit(1);
+		}
+		return s3is;
 	}
 	
 	public void s3DeleteObject(String keyName) {
--- src/main/java/com/uploader/UploaderController.java
+++ src/main/java/com/uploader/UploaderController.java
@@ -1,10 +1,8 @@
 package com.uploader;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
@@ -14,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
@@ -45,19 +44,17 @@ public class UploaderController {
 	}
 	
 	@RequestMapping(value ="/download", method = RequestMethod.GET)
-	public String downloadObject(@RequestParam("objectKey") String keyName, HttpServletResponse response) {
-		System.out.println(keyName);
+	@ResponseBody
+	public void downloadObject(@RequestParam("objectKey") String keyName, HttpServletResponse response) {
 		try {
 			S3ObjectInputStream s3is = s3Utils.s3DownloadObject(keyName);
 			response.setContentType("APPLICATION/OCTET-STREAM");
 			response.setHeader("Content-Disposition", "attachment; filename=" + keyName);
 		    IOUtils.copy(s3is, response.getOutputStream());
-		    s3is.close();
 		    response.flushBuffer();
 	    } catch (IOException ex) {
 	      throw new RuntimeException("IOError writing file to output stream");
 	    }
-		return "index";
 	}
 	
 	@RequestMapping(value ="/delete", method = RequestMethod.POST)
